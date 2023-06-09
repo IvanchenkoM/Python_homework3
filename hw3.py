1cc7544c4a28bb1ed610c82afedb3d399d2df8cc
Marina Ivanchenko <sunshine808@ya.ru>
2023-06-09 12:50:01 +0300

    1

---
+++ hw3.py
@@ -0,0 +1,15 @@
+# print('Введите первый массив: ')
+# a = [int(a) for a in input().split()]
+# print('Введите первый массив: ')
+# b = [int(a) for a in input().split()]
+
+# for i in range(len(a)):
+#     buled = True
+#     for j in range(len(b)):
+#         if a[i] == b[j]: buled = False
+#     if buled: print(a[i], ' ', end='')
+
+# a = []
+# n = int(input('Enter range A: '))
+# for i in range(n):
+#     a.append(input('Enter A: '))
